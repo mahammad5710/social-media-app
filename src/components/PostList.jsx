166ebe600b4630f1ce85f02ca9618f76166fba46
Mahammad5710 <mahammad5710@gmail.com>
2026-06-18 12:18:51 +0530

    post-list added

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -7,27 +7,27 @@ import Loader from "./Loader";
 const PostList = () => {
   const { postList, addPosts } = useContext(PostListItems);
   const [fetching, setFetching] = useState(false);
-
   useEffect(() => {
     if(postList.length > 0){
       return;
     }
-    setFetching(true);
+  setFetching(true);
 
-    const controller = new AbortController();
+  const controller = new AbortController();
 
-    fetch("https://dummyjson.com/posts", {
-      signal: controller.signal,
+  fetch("https://dummyjson.com/posts", {
+    signal: controller.signal,
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      addPosts(data.posts);
+      setFetching(false);
     })
-      .then((res) => res.json())
-      .then((data) => {
-        addPosts(data.posts);
-        setFetching(false);
-      })
-    return () => {
-      controller.abort();
-    };
-  }, []);
+
+  return () => {
+    controller.abort();
+  };
+}, []);
 
   return (
     <>
